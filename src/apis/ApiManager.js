bb928a0a77f72e1354e98abe3df3625227e25523
Thùy Nhi IT <21054781.nhi@student.iuh.edu.vn>
2025-10-04 14:40:24 +0700

    Add API notification

--- src/apis/ApiManager.js
+++ src/apis/ApiManager.js
@@ -36,6 +36,10 @@ export const ApiManager = {
         const res = await api.delete(url, { data });
         return res.data;
     },
+    patch: async (url, data) => {
+        const res = await api.patch(url, data);
+        return res.data;
+    },
 };
 
 export default ApiManager;
