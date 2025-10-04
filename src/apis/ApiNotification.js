2256330ac8da612cfbb8f968f9845b7266dd8e24
Thùy Nhi IT <21054781.nhi@student.iuh.edu.vn>
2025-10-04 12:12:06 +0700

    Add new API notification

--- src/apis/ApiNotification.js
+++ src/apis/ApiNotification.js
@@ -5,6 +5,9 @@ const ApiNotification = {
     getNotificationsByUser: () => ApiManager.get("/notifications"),
     markAsRead: (notificationId) => ApiManager.patch(`/notifications/${notificationId}/read`),
     deleteNotification: (notificationId) => ApiManager.delete(`/notifications/${notificationId}`),
+    getUnreadCount: () => ApiManager.get("/notifications/unread-count"),
+    markAllAsRead: () => ApiManager.patch("/notifications/mark-all-read"),
+
 };
 
 export default ApiNotification;
